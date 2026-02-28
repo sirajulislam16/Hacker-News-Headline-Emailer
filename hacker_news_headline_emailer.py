874e033bb376e1087398cd158531ea53ca222986
sirajulislam16 <sirajulislam160808@gmail.com>
2026-02-28 16:49:01 +0600

    added the required modules

---
+++ hacker_news_headline_emailer.py
@@ -0,0 +1,30 @@
+ # http requests
+import requests
+
+#web scraping
+from bs4 import BeautifulSoup
+
+#send the mail
+import smtplib
+
+
+#email body
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# system date and time manipulation
+import datetime
+now = datetime.datetime.now()
+
+# email content placeholder
+
+content = ' '
+
+# extracting Hacker News Stories
+def extract_news(url):
+    print("Extracting Hacker News Stories...")
+    ctn = ''
+    ctn += ('<b>HN Top Stories</b>\n'+'<br>'+'-'*50+'</br>')
+    response = requests.get(url)
+    content= response.content
+    soup = BeautifulSoup(content, 'html.parser')
