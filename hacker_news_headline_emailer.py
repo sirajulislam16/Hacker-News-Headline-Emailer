3918ed9b9c64f5c5d31bd37438de5cf9ae66c875
sirajulislam16 <sirajulislam160808@gmail.com>
2026-02-28 17:17:35 +0600

    Add Hacker News scraping function

--- hacker_news_headline_emailer.py
+++ hacker_news_headline_emailer.py
@@ -28,3 +28,9 @@ def extract_news(url):
     response = requests.get(url)
     content= response.content
     soup = BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(
+        soup.find_all('td', attrs={'class':'title','valign':''})
+    ):
+        ctn +=((str(i+1)+' :: '+tag.text+"\n"+'<br>')if tag.text != 'More' else'')
+        #print(tag.prettify) find all('span),attrs={'class':''sitestr'}
+    return(ctn)
